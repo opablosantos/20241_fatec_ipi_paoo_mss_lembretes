03063fbdff2864b47b0d5b73c9dde8efb2c06a6f
Pablo Santos <opablosantos@gmail.com>
2024-06-12 15:01:25 -0300

    feat(barramento): deixa de fazer broadcast

--- barramento/index.js
+++ barramento/index.js
@@ -2,34 +2,32 @@ require('dotenv').config()
 const express = require('express')
 const axios = require('axios')
 const app = express()
-//aplicando um middleware
+
 app.use(express.json())
 
 const eventos = []
 
-//aqui recebemos todos os eventos
-//e repassamos para todos os mss
 app.post('/eventos', async (req, res) => {
-  //aqui pegamos o evento
   const evento = req.body
   eventos.push(evento)
   console.log(evento)
-  try{
-    await axios.post('http://localhost:4000/eventos', evento)
+
+  try {
+    if (evento.type === 'ObservacaoCriada') {
+      await axios.post('http://localhost:7000/eventos', evento) // Classificação
+    }
+
+    if (evento.type === 'ObservacaoClassificada') {
+      await axios.post('http://localhost:5000/eventos', evento) // Observações
+    }
+
+    if (evento.type !== 'ObservacaoClassificada') {
+      await axios.post('http://localhost:6000/eventos', evento) // Consulta
+    }
+  } catch (e) {
+    console.error('Erro ao enviar evento:', e.message)
   }
-  catch(e){}
-  try{
-    await axios.post('http://localhost:5000/eventos', evento)
-  }
-  catch(e){}
-  try{
-    await axios.post('http://localhost:6000/eventos', evento)
-  }
-  catch(e){}
-  try{
-    await axios.post('http://localhost:7000/eventos', evento)
-  }
-  catch(e){}
+
   res.status(200).end()
 })
 
@@ -37,7 +35,6 @@ app.get('/eventos', (req, res) => {
   res.status(200).json(eventos)
 })
 
-app.listen(
-  process.env.PORT, 
-  () => console.log(`Barramento: ${process.env.PORT}`)
-)
+app.listen(process.env.PORT, () => {
+  console.log(`Barramento: ${process.env.PORT}`)
+})
